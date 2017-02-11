03369659838487e0086a20a3fe92e70dfe64d3ba
Andreas Gassmann <andreas@andreasgassmann.ch>
2017-02-11 01:49:44 +0100

    feat(app): Add ItemRepository and pass data from camera to pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -4,11 +4,14 @@ import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { UmweltPage } from '../umwelt/umwelt';
 import { MapPage } from '../map/map';
+import { NavParams } from "ionic-angular";
 
 @Component({
+  selector: 'page-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  item: any;
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
@@ -17,7 +20,7 @@ export class TabsPage {
   tab4Root: any = UmweltPage;
   tab5Root: any = MapPage;
 
-  constructor() {
-
+  constructor(private navParams: NavParams) {
+    this.item = { item: this.navParams.get('item') };
   }
 }
---
+++ src/providers/item-repository.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ItemRepository {
+  items: any = [];
+
+  constructor() {
+    this.items.push({
+      id: 7640150491001,
+      name: 'Wienerli',
+      image: 'http://static.wixstatic.com/media/8ef132_db0a53c0917c4d118061767f65a37452.png_srz_355_316_85_22_0.50_1.20_0.00_png_srz',
+      price: 1.4,
+      category: 0
+    }, {
+      id: 7610097111072,
+      name: 'Lasagne',
+      image: 'http://limon.ch/wp-content/uploads/2015/09/rivella.jpg',
+      price: 7.4,
+      category: 0
+    });
+  }
+
+  getItemByEan(ean) {
+    return this.items.filter(i => i.id === ean);
+  }
+
+}
